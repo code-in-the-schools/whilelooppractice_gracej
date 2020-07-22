92411b6ac6507107f973ecfef969b7416b06dd3d
gracejoh <ganwynj@icloud.com>
2020-07-22 17:01:39 +0000

    .,

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ r = random.randint(-100,100)
 
 found = False
 
-n = input("type a number: ")
 
 while found == False :
+  
+  n = input("type a number: ")
+  
   if n > r :
     print("the number is too high.")
   elif n < r :
     print("the number is too low.")
-  else :
-    print ("correct number")
+  else : print("invalid number")
